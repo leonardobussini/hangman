a3d7bd9e464554f96bf159639bfcd0ea527b25ec
Leonardo Bussini <leonardo.bussini@gmail.com>
2024-08-04 13:45:53 +0200

    Cancellati bin e obj

--- Hangman/obj/Debug/net8.0/Hangman.AssemblyInfo.cs
+++ Hangman/obj/Debug/net8.0/Hangman.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5d52e649ea873d277dd9416872cd7f6f1f5399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b1bcb6269f6c4ef8533c449c561504b998d0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
